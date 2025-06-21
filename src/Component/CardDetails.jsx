b90ea507130ea1823b46b29c9e00f5361b547bc4
Ahmed <Ams_jed24@hotmail.com>
2025-06-21 13:59:35 +0300

    Done

--- src/Component/CardDetails.jsx
+++ src/Component/CardDetails.jsx
@@ -3,17 +3,17 @@ function CardDetails(props) {
 
     return (
        <>
-             <div className='card bg-slate-700 text-center w-4/12 m-auto mt-60 h-fit flex flex-col  rounded-2xl relative'>
+             <div className='card bg-slate-700 text-center w-4/12 m-auto mt-60 h-fit flex flex-col  rounded-2xl relative shadow-2xl max-sm:w-11/12 max-lg:w-7/12 max-xl:w-5/12 max-2xl:w-5/12 font-mono'>
         <p className='text-green-400 pt-5 text-[15px]'>ADVICE #{props.number}</p>
         <div className='flex justify-center items-center w-3/4 m-auto'>
-        <p className='text-stone-200 p-5 font-bold'> {props.advice}</p>
+        <p className='text-stone-200 p-5 font-bold'> "{props.advice}"</p>
             </div >
             <div className="pb-17 w-3/4 m-auto ">
             <img src="src/assets/pattern-divider-desktop.svg" alt="" className='w-full'/>
             </div>
             <div className='absolute bottom-[-20px] left-0 right-0 '> 
-      <div className='bg-green-400 rounded-full w-16 h-16 m-auto'>
-        <img src="src/assets/icon-dice.svg" alt="" className=' m-auto pt-5 w-1/3'/>
+      <div className='bg-green-400 rounded-full w-16 h-16 m-auto hover:cursor-pointer  ' onClick={() => window.location.reload()}>
+        <img src="src/assets/icon-dice.svg" alt="" className=' m-auto pt-5 w-1/3 '/>
       </div>
     </div>
       </div>
